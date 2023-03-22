5c207a5e740447c45abbafc6dc750b27c353a284
NightMareRahul <nightmarecoding0001@gmail.com>
2023-03-22 20:01:25 +0530

    uploaded

--- attendence.py
+++ attendence.py
@@ -1,15 +1,17 @@
 import db_conn
 import db_execute_query
+import db_data_unpack
+import datetime
 
-def markAttendance(ID):
+def enter_attendence(ID, connection) :
     query_1 = """
     select Name from images where id = %s;
     """
-    connection = db_conn.create_db_connection("localhost", "root", "", "person")
+        
     results = db_execute_query.read_query(connection, query_1, ID)
 
-    tuple = results[0]
-    name, = tuple
+    # Fetching Name from database...
+    name = db_data_unpack.unpack(results)
 
     query_2 = """
     insert into attendence(id, name) values (%s, %s)
@@ -17,3 +19,30 @@ def markAttendance(ID):
     data = (ID, name)
 
     db_execute_query.execute_query(connection, query_2, data)
+
+def markAttendance(ID) :
+
+    system_time_stamp = datetime.datetime.now()
+    connection = db_conn.create_db_connection("localhost", "root", "", "person")
+
+    query_1 = """
+    select login_info from attendence where id = %s order by sl_no desc limit 1
+    """
+
+    time_stamp = db_execute_query.read_query(connection, query_1, ID)
+
+    if not time_stamp :
+        enter_attendence(ID, connection)
+        exit()
+    
+    time_stamp = db_data_unpack.unpack(time_stamp)
+
+    time_diff = system_time_stamp - time_stamp
+    time_diff = time_diff.total_seconds() / 60 ** 2
+
+    if time_diff > 1 :
+        enter_attendence(ID, connection)
+
+
+# Testing....
+markAttendance(3)
---
+++ db_data_unpack.py
@@ -0,0 +1,4 @@
+def unpack(val) :
+    tuple = val[0]
+    data, = tuple
+    return data
