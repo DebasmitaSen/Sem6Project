0741c36471f2b187b28a1587ddca7ee45323da34
NightMareRahul <pal521843@gmail.com>
2023-04-12 11:17:01 +0530

    updated

--- recogniy.py
+++ recogniy.py
@@ -38,13 +38,11 @@ def createImg():
     getdata=getArray()
     list1=getdata[0]
     var="/img"
-    count=1
     list2=[]
     list3=[]
     for i in  range (1,6):
         cn="%s"%i
         var2=var+cn+".jpg"
-        #i=i+1
         list2.append(var2)
     for i in range (len(list1)):
         for j in range (len(list2)):
@@ -67,12 +65,15 @@ def createImg():
 def recogniseImg():
     # cap  = cv2.VideoCapture("http://100.81.178.96:4747/video?640x480")
     cap  = cv2.VideoCapture(0)
+    getdata1=getArray()
+    name2=getdata1[1]
+    classid=getdata1[2]
+    encoded_face_train=createImg()
     while True:
         success, img = cap.read()
         if not success:
             break
-        else:
-            encoded_face_train=createImg()
+        else:           
             imgS = cv2.resize(img, (0,0), None, 0.25,0.25)
             imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
             faces_in_frame = face_recognition.face_locations(imgS)
@@ -81,13 +82,8 @@ def recogniseImg():
                 matches = face_recognition.compare_faces(encoded_face_train, encode_face)
                 faceDist = face_recognition.face_distance(encoded_face_train, encode_face)
                 matchIndex = np.argmin(faceDist)
-                print(matchIndex)
-                
-                if matches[matchIndex]:
-                    getdata1=getArray()
-                    name2=getdata1[1]
-                    classid=getdata1[2]
-                    
+                print(matchIndex)               
+                if matches[matchIndex]:                   
                     name= name2[matchIndex]
                     iD = classid[matchIndex]
                     y1,x2,y2,x1 = faceloc
@@ -97,7 +93,7 @@ def recogniseImg():
                     cv2.rectangle(img, (x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
                     cv2.putText(img,"Detected", (x1+6,y2-5), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
                     #call mark attendance
-                    attendence.markAttendance(iD)
+                    # attendence.markAttendance(iD)
                     print(iD, name)
                 else:
                     y1,x2,y2,x1 = faceloc
@@ -106,8 +102,11 @@ def recogniseImg():
                     cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
                     cv2.rectangle(img, (x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
                     cv2.putText(img,"Unrecognizable", (x1+6,y2-5), cv2.FONT_HERSHEY_COMPLEX,0.50,(0,0,255),2)
-                return (iD, name)
-                    
+                # return (iD, name)
+            cv2.imshow('webcam', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+              
            
             
     
@@ -115,4 +114,4 @@ def recogniseImg():
 
 
 
-# recogniseImg()
+recogniseImg()
