35e542fe42e2b11b252a6d2b71f3b2543aa84341
NightMareRahul <pal521843@gmail.com>
2023-04-24 21:47:34 +0530

    updated

--- Database/person.sql
+++ Database/person.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2023 at 06:17 PM
+-- Generation Time: Apr 24, 2023 at 06:17 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -33,17 +33,6 @@ CREATE TABLE `attendence` (
   `name` varchar(30) NOT NULL,
   `login_info` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `attendence`
---
-
-INSERT INTO `attendence` (`sl_no`, `id`, `name`, `login_info`) VALUES
-(15, 14, 'Debu', '2023-03-21 14:58:44'),
-(21, 14, 'Debu', '2023-03-22 15:29:39'),
-(24, 3, 'Rahul', '2023-03-22 15:52:31'),
-(25, 14, 'Debu', '2023-03-22 18:37:01'),
-(26, 3, 'Rahul', '2023-03-22 18:37:33');
 
 -- --------------------------------------------------------
 
@@ -85,7 +74,9 @@ CREATE TABLE `total_attendence` (
 
 INSERT INTO `total_attendence` (`id`, `name`, `total_att`) VALUES
 (3, 'Rahul', 2),
-(14, 'Debu', 3);
+(14, 'debu', 6),
+(20, 'rajdip', 4),
+(43, 'Romit', 0);
 
 --
 -- Indexes for dumped tables
@@ -117,7 +108,7 @@ ALTER TABLE `total_attendence`
 -- AUTO_INCREMENT for table `attendence`
 --
 ALTER TABLE `attendence`
-  MODIFY `sl_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `sl_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
