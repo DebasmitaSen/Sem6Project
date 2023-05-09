8f23782e60be50a0b9fa9abc88456b1d62d8643c
NightMareRahul <nightmarecoding0001@gmail.com>
2023-05-09 17:45:08 +0530

    updated

---
+++ findencoding.py
@@ -0,0 +1,47 @@
+import cv2
+import face_recognition
+import numpy as np
+import image_manupulation
+
+def find_encoding():
+    arrays = image_manupulation.retrive()
+    classID=arrays[0]
+    names=arrays[1]
+    images=arrays[2]
+    name2=[]
+    classid=[]
+    for i in range(len(names)):
+        for j in range(5):
+            name2.append(names[i])
+            classid.append(classID[i])
+
+    list1 = images
+    var="/img"
+    list2=[]
+    list3=[]
+    for i in  range (1,6):
+        cn="%s"%i
+        var2=var+cn+".jpg"
+        list2.append(var2)
+    for i in range (len(list1)):
+        for j in range (len(list2)):
+            var3=list1[i]+list2[j]
+            list3.append(var3)
+    imagelist=[]
+
+    for i in range(len(names) * 5):
+        path=list3[i]
+        imgx=cv2.imread(path)
+        imagelist.append(imgx)
+
+    def findEncodings(image):
+        encodeList = []
+        for img in image:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
+            encoded_face = face_recognition.face_encodings(img)[0]
+            encodeList.append(encoded_face)
+        return encodeList
+
+
+    encoded_face_train = findEncodings(imagelist)
+    return encoded_face_train, name2, classid
--- recogniy.py
+++ recogniy.py
@@ -1,58 +1,55 @@
 import cv2
 import face_recognition
 import numpy as np
-import image_manupulation
 import attendence
-
+import findencoding
 
 #code to connect to database and collect images and the id of student
 #array name must be 'images'
 #array for id must be 'classID'
-arrays = image_manupulation.retrive()
-classID=arrays[0]
-names=arrays[1]
-images=arrays[2]
-name2=[]
-classid=[]
-for i in range(len(names)):
-    for j in range(5):
-        name2.append(names[i])
-        classid.append(classID[i])
+# arrays = image_manupulation.retrive()
+# classID=arrays[0]
+# names=arrays[1]
+# # images=arrays[2]
+# name2=[]
+# classid=[]
+# for i in range(len(names)):
+#     for j in range(5):
+#         name2.append(names[i])
+#         classid.append(classID[i])
+
+# list1 = images
+# var="/img"
+# list2=[]
+# list3=[]
+# for i in  range (1,6):
+#     cn="%s"%i
+#     var2=var+cn+".jpg"
+#     list2.append(var2)
+# for i in range (len(list1)):
+#     for j in range (len(list2)):
+#         var3=list1[i]+list2[j]
+#         list3.append(var3)
+# imagelist=[]
+
+# for i in range(len(names) * 5):
+#     path=list3[i]
+#     imgx=cv2.imread(path)
+#     imagelist.append(imgx)
+
+# def findEncodings(image):
+#     encodeList = []
+#     for img in image:
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
+#         encoded_face = face_recognition.face_encodings(img)[0]
+#         encodeList.append(encoded_face)
+#     return encodeList
 
 
-def findEncodings(image):
-    encodeList = []
-    for img in image:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
-        encoded_face = face_recognition.face_encodings(img)[0]
-        encodeList.append(encoded_face)
-    return encodeList
 
+# encoded_face_train = findEncodings(imagelist)
 
-
-list1 = images
-var="/img"
-list2=[]
-list3=[]
-for i in  range (1,6):
-    cn="%s"%i
-    var2=var+cn+".jpg"
-    list2.append(var2)
-for i in range (len(list1)):
-    for j in range (len(list2)):
-        var3=list1[i]+list2[j]
-        list3.append(var3)
-imagelist=[]
-
-for i in range(len(names) * 5):
-    path=list3[i]
-    imgx=cv2.imread(path)
-    imagelist.append(imgx)
-
-
-encoded_face_train = findEncodings(imagelist)
-
-
+name2, classid, encoded_face_train = findencoding.find_encoding()
 ###### take pictures from device
 def recogniseImg(cap):
 
@@ -65,7 +62,7 @@ def recogniseImg(cap):
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
         faces_in_frame = face_recognition.face_locations(imgS)
         encoded_faces = face_recognition.face_encodings(imgS, faces_in_frame)
-        for encode_face, faceloc in zip(encoded_faces,faces_in_frame):
+        for encode_face in zip(encoded_faces,faces_in_frame):
             matches = face_recognition.compare_faces(encoded_face_train, encode_face)
             faceDist = face_recognition.face_distance(encoded_face_train, encode_face)
             print(faceDist)
